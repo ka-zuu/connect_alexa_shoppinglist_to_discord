71f9e0e7d894b6bdc2c4ed5228f8dde34670788a
kazuu <3580752+ka-zuu@users.noreply.github.com>
2023-01-28 21:14:49 +0900

    標準出力もログに書き込むように修正

--- shoppinglist_to_discord_update.sh
+++ shoppinglist_to_discord_update.sh
@@ -8,7 +8,7 @@ tmp=$(mktemp)
 work_dir="$(dirname $0)" # スクリプトのあるディレクトリ
 filename="$(basename $0)" # スクリプトのファイル名
 mkdir -p ${work_dir}/log # ログディレクトリ
-exec 2> ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) # 標準エラー出力をログファイルに出力
+exec > ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) 2>&1 # 標準出力とエラー出力をログファイルに出力
 cd ${work_dir}
 
 # 外部の変数ファイルを取得
